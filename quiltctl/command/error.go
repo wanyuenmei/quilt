ccfc1dccdb356c90f7b0489a32f9b7bf3a80e5b2
Michael Zhao <mengzhe.zhao@gmail.com>
2016-10-23 20:20:19 -0700

    quiltctl: Remind user to start daemon before running a subcommand
    Before, when a user tried to run a spec (or other subcommand) before
    starting the daemon, they would only get an error message reporting
    that they were unable to connect to the daemon. Now, they will also
    receive a reminder to start the daemon.

--- quiltctl/command/error.go
+++ quiltctl/command/error.go
@@ -11,6 +11,7 @@ type DaemonConnectError struct {
 }
 
 func (err DaemonConnectError) Error() string {
-	return fmt.Sprintf("Unable to connect to the Quilt daemon at %s: %s.",
-		err.host, err.connectError.Error())
+	return fmt.Sprintf("Unable to connect to the Quilt daemon at %s: %s. "+
+		"Is the quilt daemon running? If not, you can start it with "+
+		"`quilt daemon`.", err.host, err.connectError.Error())
 }
