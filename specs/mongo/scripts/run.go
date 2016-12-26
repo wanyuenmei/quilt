5a476d670bfb3be04bd84c7d95e5b5062de291a9
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-12-26 13:47:40 -0500

    mongo: Refactor the Mongo DB run script
    The mongo db run script had several problems which this patch fixes.
    First, it waited for the network to come up before starting `mongod`.
    This was no longer necessary, and failed occasionally due to a
    peculiarity in the go resolver code.  Second, it misconfigured the
    Mongo DB to bind to the IPs of remote instances, and finally third, it
    was much more complex than necessary.

--- specs/mongo/scripts/run.go
+++ specs/mongo/scripts/run.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -32,134 +29,49 @@ func main() {
 	peersVar := os.Getenv("PEERS")
 	peers := strings.Split(peersVar, ",")
 
-	addrs := resolveHostname(host)
-
-	if role == initialRole {
-		for _, peer := range peers {
-			checkConnection(fmt.Sprintf("%s:27017", peer))
-		}
-	}
-
-	var wg sync.WaitGroup
-
 	log.Printf("Starting Mongo server")
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if err := runServer(addrs); err != nil {
-			log.Fatalf("Error running Mongo server: %s", err.Error())
-		}
-	}()
+	cmd := exec.Command("mongod", "--replSet", "rs0")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Start()
+	defer cmd.Wait()
 
-	if role == initialRole && len(peers) > 0 {
-		checkConnection(fmt.Sprintf("%s:27017", host))
+	if role != initialRole || len(peers) == 0 {
+		return
+	}
 
+	// At this point, mongod may or may not have gotten to the point where it can
+	// receive instructions to configure the replica set.  Thus, we have to wait
+	// until its ready by sleeping.
+	for {
+		time.Sleep(2 * time.Second)
 		log.Printf("Setting up replica set with peers: %+v", peers)
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if err := setupReplicaSet(peers); err != nil {
-				log.Fatalf("Error creating replica sets: %s", err.Error())
+		if err := setupReplicaSet(host, peers); err != nil {
+			log.Printf("Failed setup replica set: %s", err)
+		} else {
+			break
+		}
+	}
+}
+
+func setupReplicaSet(host string, peers []string) error {
+	rsInit := `
+		rs.initiate(
+			{
+				_id: "rs0",
+				version: 1,
+				members: [%s]
 			}
-		}()
+		)`
+
+	var members []string
+	for i, m := range append([]string{host}, peers...) {
+		members = append(members, fmt.Sprintf(`{_id: %d, host : "%s"}`, i, m))
 	}
 
-	wg.Wait()
-}
-
-type mongoConfig struct {
-	Net         netCfg `json:"net"`
-	Replication rsCfg  `json:"replication"`
-}
-
-type netCfg struct {
-	IPs string `json:"bindIp"`
-}
-
-type rsCfg struct {
-	ReplicaSetName string `json:"replSetName"`
-}
-
-func runServer(addrs []string) error {
-	path := "/etc/mongod.conf"
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	b, err := json.Marshal(buildMongoConfig(addrs))
-	if err != nil {
-		return err
-	}
-
-	if _, err := f.Write(b); err != nil {
-		return err
-	}
-
-	cmd := exec.Command("mongod", "--config", path)
+	rsInit = fmt.Sprintf(rsInit, strings.Join(members, ","))
+	cmd := exec.Command("mongo", "--eval", rsInit)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
-}
-
-func buildMongoConfig(addrs []string) mongoConfig {
-	ips := append([]string{"127.0.0.1"}, addrs...)
-	return mongoConfig{
-		Net: netCfg{
-			IPs: strings.Join(ips, ","),
-		},
-		Replication: rsCfg{
-			ReplicaSetName: "rs0",
-		},
-	}
-}
-
-func setupReplicaSet(hosts []string) error {
-	if err := executeMongoCommand("rs.initiate()"); err != nil {
-		return err
-	}
-
-	for _, host := range hosts {
-		err := executeMongoCommand(fmt.Sprintf("rs.add('%s')", host))
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func executeMongoCommand(op string) error {
-	cmd := exec.Command("mongo", "--eval", op)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func resolveHostname(hostname string) []string {
-	for {
-		addrs, err := net.LookupHost(hostname)
-		if err == nil {
-			log.Printf("Resolved %s to %+v", hostname, addrs)
-			return addrs
-		}
-
-		log.Printf("Unable to resolve %s: %s. Retrying...", hostname, err.Error())
-		time.Sleep(time.Second)
-	}
-}
-
-func checkConnection(address string) {
-	for {
-		conn, err := net.Dial("tcp", address)
-		if err == nil {
-			conn.Close()
-			log.Printf("Resolved %s", address)
-			return
-		}
-
-		log.Printf("Unable to resolve %s: %s. Retrying...", address, err.Error())
-		time.Sleep(time.Second)
-	}
 }
--- specs/mongo/scripts/run_test.go
+++ specs/mongo/scripts/run_test.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func TestBuildMongoConfig(t *testing.T) {
-	ip := []string{"0.0.0.0", "127.0.0.1", "192.168.52.7"}
-	assert.EqualValues(t, buildMongoConfig(ip), mongoConfig{
-		Net:         netCfg{IPs: "127.0.0.1,0.0.0.0,127.0.0.1,192.168.52.7"},
-		Replication: rsCfg{ReplicaSetName: "rs0"},
-	})
+func TestNothing(t *testing.T) {
 }
