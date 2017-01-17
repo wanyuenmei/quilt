0c09f75031bf73f21523285b6a914c3cfb957f59
Tejas Manohar <tejas@segment.com>
2017-01-16 19:11:15 -0800

    google: Test List()
    This commit scaffolds the GCE provider test suite and tests the
    public List() method as an example.

--- cluster/google/google_test.go
+++ cluster/google/google_test.go
@@ -1,6 +1,67 @@
 package google
 
-import "testing"
+import (
+	"testing"
 
-func TestDummy(t *testing.T) {
+	"github.com/NetSys/quilt/cluster/machine"
+	"github.com/NetSys/quilt/db"
+	"github.com/stretchr/testify/suite"
+
+	compute "google.golang.org/api/compute/v1"
+)
+
+type GoogleTestSuite struct {
+	suite.Suite
+
+	gce  *mockClient
+	clst *Cluster
+}
+
+func (s *GoogleTestSuite) SetupTest() {
+	supportedZones = []string{"zone-1"}
+	s.gce = &mockClient{}
+	s.clst = &Cluster{
+		gce:    s.gce,
+		projID: "project",
+		ns:     "namespace",
+	}
+}
+
+func (s *GoogleTestSuite) TestList() {
+	s.gce.On("ListInstances", "project", "zone-1", apiOptions{
+		filter: "description eq namespace",
+	}).Return(&compute.InstanceList{
+		Items: []*compute.Instance{
+			{
+				MachineType: "machine/split/type-1",
+				Name:        "name-1",
+				NetworkInterfaces: []*compute.NetworkInterface{
+					{
+						AccessConfigs: []*compute.AccessConfig{
+							{
+								NatIP: "x.x.x.x",
+							},
+						},
+						NetworkIP: "y.y.y.y",
+					},
+				},
+			},
+		},
+	}, nil)
+
+	machines, err := s.clst.List()
+	s.NoError(err)
+	s.Len(machines, 1)
+	s.Equal(machines[0], machine.Machine{
+		ID:        "name-1",
+		PublicIP:  "x.x.x.x",
+		PrivateIP: "y.y.y.y",
+		Size:      "type-1",
+		Region:    "zone-1",
+		Provider:  db.Google,
+	})
+}
+
+func TestGoogleTestSuite(t *testing.T) {
+	suite.Run(t, new(GoogleTestSuite))
 }
