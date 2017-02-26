d53d0976fa9b49ce402443cfc44a2db4a42792cc
Kevin Lin <kaikai526@gmail.com>
2017-02-25 18:39:22 -0600

    ci: Fix elasticsearch syntax error
    `createDeployment` requires an options argument, even if it's empty.

--- quilt-tester/tests/elasticsearch/elasticsearch.js
+++ quilt-tester/tests/elasticsearch/elasticsearch.js
@@ -1,7 +1,7 @@
 var Elasticsearch = require("github.com/quilt/elasticsearch").Elasticsearch;
 var infrastructure = require("github.com/quilt/quilt/quilt-tester/config/infrastructure")
 
-var deployment = createDeployment();
+var deployment = createDeployment({});
 deployment.deploy(infrastructure);
 
 var nWorker = deployment.machines.filter(function(m) {
