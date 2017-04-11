82ac9a1dc8bda212448c231552f3eb963e762314
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2017-04-11 10:40:30 -0700

    infrastructure: Lower costs with fewer preemptible instances

--- quilt-tester/config/infrastructure.js
+++ quilt-tester/config/infrastructure.js
@@ -6,6 +6,7 @@ MachineDeployer.prototype.deploy = function(deployment) {
     var baseMachine = new Machine({
         provider: "Amazon",
         region: "us-west-1",
+        preemptible: true,
         sshKeys: ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxMuzNUdKJREFgUkSpD0OPjtgDtbDvHQLDxgqnTrZpSvTw5r8XDd+AFS6eVibBfYv1u+geNF3IEkpOklDlII37DzhW7wzlRB0SmjUtODxL5hf9hKoScDpvXG3RBD6PBCyOHA5IJBTqPGpIZUMmOlXDYZA1KLaKQs6GByg7QMp6z1/gLCgcQygTDdiTfESgVMwR1uSQ5MRjBaL7vcVfrKExyCLxito77lpWFMARGG9W1wTWnmcPrzYR7cLzhzUClakazNJmfso/b4Y5m+pNH2dLZdJ/eieLtSEsBDSP8X0GYpmTyFabZycSXZFYP+wBkrUTmgIh9LQ56U1lvA4UlxHJ"],
     });
 
@@ -14,4 +15,4 @@ MachineDeployer.prototype.deploy = function(deployment) {
 }
 
 // We will have four worker machines.
-module.exports = new MachineDeployer(4);
+module.exports = new MachineDeployer(3);
