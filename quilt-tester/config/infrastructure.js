d73759bcc38f91e63e4a73de6bfe8c34af67a7cc
Kevin Lin <kevin@quilt.io>
2017-05-25 11:12:23 -0700

    tester: Test all providers
    This patch modifies the quilt-tester spec to install credentials for
    DigitalOcean and Google (in addition to Amazon), and rotate providers
    every hour.

--- quilt-tester/config/infrastructure.js
+++ quilt-tester/config/infrastructure.js
@@ -6,15 +6,19 @@ function MachineDeployer(nWorker) {
 
 MachineDeployer.prototype.deploy = function(deployment) {
     var baseMachine = new Machine({
-        provider: "Amazon",
-        region: "us-west-1",
+        provider: process.env.PROVIDER || "Amazon",
+        size: process.env.SIZE || "m3.medium",
         sshKeys: ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxMuzNUdKJREFgUkSpD0OPjtgDtbDvHQLDxgqnTrZpSvTw5r8XDd+AFS6eVibBfYv1u+geNF3IEkpOklDlII37DzhW7wzlRB0SmjUtODxL5hf9hKoScDpvXG3RBD6PBCyOHA5IJBTqPGpIZUMmOlXDYZA1KLaKQs6GByg7QMp6z1/gLCgcQygTDdiTfESgVMwR1uSQ5MRjBaL7vcVfrKExyCLxito77lpWFMARGG9W1wTWnmcPrzYR7cLzhzUClakazNJmfso/b4Y5m+pNH2dLZdJ/eieLtSEsBDSP8X0GYpmTyFabZycSXZFYP+wBkrUTmgIh9LQ56U1lvA4UlxHJ"],
     });
 
     deployment.deploy(baseMachine.asMaster());
 
     var worker = baseMachine.asWorker();
-    worker.preemptible = true;
+    // Preemptible instances are currently only implemented on Amazon.
+    if (baseMachine.provider === "Amazon") {
+        worker.preemptible = true;
+    }
+
     deployment.deploy(worker.replicate(this.nWorker));
 }
 
